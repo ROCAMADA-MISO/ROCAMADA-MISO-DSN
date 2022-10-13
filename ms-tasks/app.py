7cf4d3d88be03c33b085a379ddc397b5f9c50dcc
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2022-10-12 23:16:44 -0500

    ✨ Add create task endpoint

--- ms-tasks/app.py
+++ ms-tasks/app.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask
+import time
+from datetime import datetime
 from dotenv import load_dotenv
-from flask_restful import Api
+from flask import Flask, request
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 
 load_dotenv()
 
@@ -18,28 +20,57 @@ ma = Marshmallow(app)
 jwt = JWTManager(app)
 api = Api(app)
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filename = db.Column(db.String(100))
     new_format = db.Column(db.String(10))
     status = db.Column(db.String(15))
     timestamp = db.Column(db.DateTime(timezone=False))
+    user_id = db.Column(db.Integer)
 
 
 class TaskSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ("id", "filename", "new_format", "status", "timestamp")
+        fields = ("id", "filename", "new_format",
+                  "status", "timestamp", "user_id")
 
 
 task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
 
 with app.app_context():
     db.create_all()
 
 
-@app.route('/api/ms-tasks')
-def hello():
-    return 'Hello, World!'
+class TasksResource(Resource):
+    @jwt_required()
+    def post(self):
+        new_format = request.form['newFormat']
+        file = request.files['fileName']
+        user_id = get_jwt_identity()
+
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        filename = file.filename.split(".")[0]
+        format = file.filename.split(".")[1]
+        filename = "{}_{}.{}".format(filename, timestr, format)
+        file.save(filename)
+
+        new_task = Task(
+            filename=filename,
+            new_format=new_format,
+            status="uploaded",
+            timestamp=datetime.strptime(datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S'),
+            user_id=user_id)
+
+        db.session.add(new_task)
+        db.session.commit()
+
+        return {"message": "Tarea creada exitosamente"}, 200
+
+
+api.add_resource(TasksResource, '/api/tasks')
 
 
 def return_app():
