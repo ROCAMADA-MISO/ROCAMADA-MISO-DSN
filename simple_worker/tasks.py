83c1488ddcb0d7f72776d5d236823078eef384b8
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-10-21 01:10:03 -0300

    task send_email

--- simple_worker/tasks.py
+++ simple_worker/tasks.py
@@ -1,3 +1,4 @@
+import os
 import time
 import datetime
 from celery import Celery
@@ -5,12 +6,15 @@ from celery.utils.log import get_task_logger
 from celery.schedules import crontab
 from pydub import AudioSegment
 from email.message import EmailMessage
+from dotenv import load_dotenv
 import smtplib
 import psycopg2
 
+load_dotenv()
+
 logger = get_task_logger(__name__)
 
-app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+app = Celery('tasks', broker=os.environ['REDIS'], backend='redis://redis:6379/0')
 
 @app.task()
 def audio_converter(filename,new_format):
@@ -20,3 +24,24 @@ def audio_converter(filename,new_format):
     audio = AudioSegment.from_file(src)
     audio.export(dst, format=new_format)
     return "New Format is ready to be downloaded"
+
+@app.task()
+def send_email(userid):
+    logger.info('Got Request - Starting work ')
+    From = os.environ['FROM_EMAIL']
+    To = os.environ['TO_EMAIL']
+    message = "¡Hola " + str(userid) + ", conversión de archivo terminada!"
+    email = EmailMessage()
+    email["From"] = From
+    email["To"] = To
+    email["Subject"] = "Correo de notificación"
+    email.set_content(message)
+    try:
+        smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+        smtp.login(From, os.environ['EMAIL_PASSWORD'])
+        smtp.sendmail(From, To, email.as_string())
+        smtp.quit()
+        logger.info('Mail Finished ')
+        return 'Email sent!'
+    except:
+        return 'Something went wrong...'  
