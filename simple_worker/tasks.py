dc473639718491a14e04f18b6fe75485827268ca
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-10-21 00:46:57 -0300

    task audio_converter

--- simple_worker/tasks.py
+++ simple_worker/tasks.py
@@ -11,3 +11,12 @@ import psycopg2
 logger = get_task_logger(__name__)
 
 app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
+@app.task()
+def audio_converter(filename,new_format):
+    logger.info('Got Request - Starting work ')
+    src = "/simple_worker/data/"+filename
+    dst = "/simple_worker/data/"+filename.split(".")[0] + "."+ new_format
+    audio = AudioSegment.from_file(src)
+    audio.export(dst, format=new_format)
+    return "New Format is ready to be downloaded"
