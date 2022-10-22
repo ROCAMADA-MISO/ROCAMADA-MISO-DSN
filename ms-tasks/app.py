f556471350554b7e209cbf3b16e55b92139cf954
Camilo Alejandro Sánchez Cruz <ca.sanchezc12@uniandes.edu.co>
2022-10-22 13:37:26 -0500

    Release 1.0 (#9)
    * 🎉 Setup project
    
    * 🔒️ Add nginx
    
    * Authentication (#2)
    
    * ✨ Add authentication microservice
    
    * 🔥 Delete user schema
    
    * ♻️ Remove template and add tasks microservice (#3)
    
    * Create and edit tasks (#4)
    
    * ✨ Add create task endpoint
    
    * ✨ Add edit endpoint for update task
    
    * ✨ Add delete task and get file endpoints (#6)
    
    * se actualiza ignore
    
    * se agrega pydub
    
    * Obtener tareas por ID de tarea y obtener todas las tareas (#7)
    
    * Obtener tareas por ID de tarea y obtener todas las tareas
    
    * 🔇 Remove logs
    
    Co-authored-by: Camilo Sanchez <ca.sanchezc12@uniandes.edu.co>
    
    * ✨ Add flag feature
    
    * Feature/converter notification (#8)
    
    * Se agrega dependencia ffmpeg
    
    * Se agrega celery
    
    * task audio_converter
    
    * task send_email
    
    * task upload_status
    
    * task get_info_user
    
    * Se agrega flag email
    
    * task upload flag exceeded
    
    * fix dbs worker
    
    * FIX
    
    * 🔥 Remove audio files
    
    Co-authored-by: Roberto Parra <w.parrav@uniandes.edu.co>
    Co-authored-by: Lizeth Daniela Castellanos Alfonso <ld.castellanos@uniandes.edu.co>
    Co-authored-by: Manuel Felipe Bello Lopez <m.bello@uniandes.edu.co>

---
+++ ms-tasks/app.py
@@ -0,0 +1,208 @@
+import os
+import time
+from datetime import datetime
+import datetime as dt
+from dotenv import load_dotenv
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask import Flask, request, send_file
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
+import json
+from celery import Celery
+
+load_dotenv()
+
+app = Flask(__name__)
+simple = Celery('simple_worker', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_TASKS_URI']
+app.config["JWT_SECRET_KEY"] = os.environ['JWT_SECRET']
+#app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+jwt = JWTManager(app)
+api = Api(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    filename = db.Column(db.String(100))
+    new_format = db.Column(db.String(10))
+    status = db.Column(db.String(15))
+    timestamp = db.Column(db.DateTime(timezone=False))
+    user_id = db.Column(db.Integer)
+
+class Flag(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    exceeded = db.Column(db.Boolean)
+
+class TaskSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        fields = ("id", "filename", "new_format",
+                  "status", "timestamp", "user_id")
+
+class FlagSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        fields = ("id","exceeded")
+
+
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+flag_schema = FlagSchema()
+
+with app.app_context():
+    db.create_all()
+    if(len(Flag.query.all()) == 0):
+        task = Flag(exceeded=False)
+        db.session.add(task)
+        db.session.commit()
+
+
+class TasksResource(Resource):
+    @jwt_required()
+    def post(self):
+        flag = Flag.query.first()
+        if(flag.exceeded):
+            return "A file start processing time has exceeded 10 minutes", 400
+        new_format = request.form['newFormat']
+        file = request.files['fileName']
+        user_id = get_jwt_identity()
+        start = time.time()
+        timestr = dt.datetime.fromtimestamp(start).strftime('%Y%m%d-%H%M%S')
+        filename = file.filename.split(".")[0]
+        format = file.filename.split(".")[1]
+        filename = "{}_{}.{}".format(filename, timestr, format)
+        file.save(filename)
+
+        new_task = Task(
+            filename=filename,
+            new_format=new_format,
+            status="uploaded",
+            timestamp=datetime.strptime(datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S'),
+            user_id=user_id)
+
+        db.session.add(new_task)
+        db.session.commit()
+        app.logger.info("Invoking task audio_converter")
+        r = simple.send_task('tasks.audio_converter', kwargs={'filename': filename, 
+                                                              'new_format': new_format,
+                                                              'userid': user_id,
+                                                              'timestamp': start
+                                                              })
+        app.logger.info(r.backend)
+
+        return {"message": "Tarea creada exitosamente"}, 200
+
+    @jwt_required()
+    def get(self):
+        order = None
+        limit = None
+        args = request.args
+        try:
+            order = int(args.get('order'))
+            limit = int(args.get('limit'))
+        except ValueError:
+            return "Invalid parameters", 400
+
+        if (order == 1):
+            user_id = get_jwt_identity()
+            task = Task.query.filter(Task.user_id == user_id).all()
+        elif (order == 0):
+            user_id = get_jwt_identity()
+            task = Task.query.filter(Task.user_id == user_id).order_by(
+                Task.id.asc()).limit(limit).all()
+        else:
+            return "Nothing to do", 400
+
+        if task is None:
+            return "Task not found", 404
+
+        return tasks_schema.dump(task, many=True), 200
+
+
+class TaskResource(Resource):
+    @jwt_required()
+    def put(self, task_id):
+        new_format = request.json['newFormat']
+        user_id = get_jwt_identity()
+        task = Task.query.filter(
+            Task.id == task_id, Task.user_id == user_id).first()
+
+        if task is None:
+            return "Task not found", 404
+        if task.new_format == new_format:
+            return "Nothing to do", 400
+
+        if task.status == "uploaded":
+            task.new_format = new_format
+            db.session.commit()
+        if task.status == "processed":
+            os.remove(
+                "./{}.{}".format(task.filename.split(".")[0], task.new_format))
+
+            task.new_format = new_format
+            task.status = "uploaded"
+            db.session.commit()
+
+        return task_schema.dump({
+            "id": task.id,
+            "filename": task.filename,
+            "new_format": task.new_format,
+            "status": task.status,
+            "timestamp": task.timestamp,
+            "user_id": task.user_id
+        }), 200
+
+    @jwt_required()
+    def delete(self, task_id):
+        user_id = get_jwt_identity()
+        task = Task.query.filter(
+            Task.id == task_id, Task.user_id == user_id).first()
+
+        if task is None:
+            return "Task not found", 404
+
+        os.remove("./{}".format(task.filename))
+
+        if task.status == "processed":
+            os.remove("./{}.{}".format(task.filename, task.new_format))
+
+        Task.query.filter(Task.id == task_id, Task.user_id == user_id).delete()
+        db.session.commit()
+        return
+
+    @jwt_required()
+    def get(self, task_id):
+        user_id = get_jwt_identity()
+        task = Task.query.filter(
+            Task.id == task_id, Task.user_id == user_id).first()
+
+        if task is None:
+            return "Task not found", 404
+
+        return task_schema.dump({
+            "id": task.id,
+            "filename": task.filename,
+            "new_format": task.new_format,
+            "status": task.status,
+            "timestamp": task.timestamp,
+            "user_id": task.user_id
+        }), 200
+
+
+class FileResource(Resource):
+    @jwt_required()
+    def get(self, filename):
+        return send_file("./{}".format(filename), download_name=filename)
+
+
+api.add_resource(TasksResource, '/api/tasks')
+api.add_resource(TaskResource, '/api/tasks/<int:task_id>')
+api.add_resource(FileResource, '/api/files/<string:filename>')
+
+
+def return_app():
+    return app
---
+++ simple_worker/tasks.py
@@ -0,0 +1,108 @@
+import os
+import time
+import datetime
+import json
+from celery import Celery
+from celery.utils.log import get_task_logger
+#from celery.schedules import crontab
+from pydub import AudioSegment
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import smtplib
+import psycopg2
+
+load_dotenv()
+
+logger = get_task_logger(__name__)
+
+app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
+
+@app.task()
+def audio_converter(filename,new_format,userid,timestamp):
+    logger.info('Got Request - Starting work ')
+    start = timestamp
+    done = time.time()
+    elapsed = done - start
+    if elapsed/60 > 10:
+        update_flag.delay()
+        return logger.info('Task took too long to complete '+ str(elapsed/60))
+    src = "/simple_worker/data/"+filename
+    dst = "/simple_worker/data/"+filename.split(".")[0] + "."+ new_format
+    audio = AudioSegment.from_file(src)
+    audio.export(dst, format=new_format)
+    if os.environ['EMAIL_SEND'] == 'True':
+        msn = get_info_user.delay(userid)
+        send_email.delay(filename)
+    upload_status.delay(filename)
+    return "New Format is ready to be downloaded"
+    
+
+@app.task()
+def update_flag():
+    conn = psycopg2.connect(host='tasks-db',
+                            database='tasks',
+                            user='postgres',
+                            port=5432,
+                            password='postgres')
+    cur = conn.cursor()
+    cur.execute("UPDATE flag SET exceeded = true")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return "Flag updated"
+
+
+@app.task()
+def get_info_user(userid):
+    logger.info('Conection to DB')
+    conn = psycopg2.connect(host='users-db',
+                            database="users",
+                            user='postgres',
+                            port=5432,
+                            password='postgres')
+    cur = conn.cursor()
+    cur.execute('SELECT username, email FROM "user" WHERE id = %s',[userid])
+    info = cur.fetchone()
+    cur.close()
+    logger.info('Info User, OK')    
+    return info
+
+@app.task()
+def send_email(filename):
+    logger.info('Got Request - Starting work ')
+    From = os.environ['FROM_EMAIL']
+    To = os.environ['TO_EMAIL']
+    #info=json.dumps(list(msn))
+    #print(info)
+    username = 'Roberto'
+    message = "¡Hola " + str(username) + ", la conversión del archivo "+ filename + " está lista para descargar!"
+    email = EmailMessage()
+    email["From"] = From
+    email["To"] = To
+    email["Subject"] = "Correo de notificación"
+    email.set_content(message)
+    try:
+        smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+        smtp.login(From, os.environ['EMAIL_PASSWORD'])
+        smtp.sendmail(From, To, email.as_string())
+        smtp.quit()
+        logger.info('Mail Finished ')
+        return 'Email sent!'
+    except:
+        return 'Something went wrong...'
+    
+@app.task()
+def upload_status(filename):
+    logger.info('Conection to DB')
+    conn = psycopg2.connect(host='tasks-db',
+                            database='tasks',
+                            user='postgres',
+                            port=5432,
+                            password='postgres')
+    cur = conn.cursor()
+    cur.execute("UPDATE task SET status=(%s)"
+                " WHERE filename = (%s)", ("processed",filename,));
+    conn.commit()
+    cur.close()
+    logger.info('Updated task status')
+    return "Status updated"
